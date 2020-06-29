3364db2bb67af11c1bb107e5cbbf519b2e82ce95
Jatin Sachdeva <jatinsachdeva931@gmail.com>
2020-06-29 12:47:14 +0530

    Added Spash Activity

---
+++ app/src/main/java/com/example/playout/SplashActivity.kt
@@ -0,0 +1,32 @@
+package com.example.playout
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.widget.ImageView
+import kotlinx.android.synthetic.main.activity_splash.view.*
+import java.lang.Exception
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val background = object : Thread() {
+            override fun run() {
+                try {
+                    sleep(4000)
+
+                    intent = Intent(baseContext, HomeActivity::class.java)
+                    startActivity(intent)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
+        background.start()
+
+    }
+}
